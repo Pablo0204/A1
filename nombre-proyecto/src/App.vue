8f49789b75c506338e629a0fdc898cba14005aef
Pablo0204 <pabloapala@alu.unach.cl>
2026-08-18 12:28:11 -0400

    Actividad 2 - Componentes y Props

--- nombre-proyecto/src/App.vue
+++ nombre-proyecto/src/App.vue
@@ -1,7 +1,15 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
-<template>
-  <HelloWorld />
+template>
+  <div id="app">
+    <Padre />
+  </div>
 </template>
+
+<script>
+import Padre from './components/Padre.vue'
+
+export default {
+  components: {
+    Padre
+  }
+}
+</script>
